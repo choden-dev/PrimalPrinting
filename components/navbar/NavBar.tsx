22ae5624aa890a14d6b998827f27b243d4354bf8
bcho892 <bcho892@aucklanduni.ac.nz>
2022-07-28 22:10:17 +1200

    initial commit

---
+++ components/navbar/NavBar.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+    Box,
+    Text,
+    Image,
+    IconButton,
+    ButtonGroup
+} from '@chakra-ui/react'
+import styles from './NavBar.module.css'
+import InstagramIcon from '../Icons/InstagramIcon'
+import FacebookIcon from '../Icons/FacebookIcon'
+type Props = {}
+
+export default function NavBar({ }: Props) {
+    return (
+        <Box
+            display="flex"
+            height="5rem"
+            width="100vw"
+            backgroundColor="white"
+            position="fixed"
+            top="0"
+            zIndex="999"
+            left="0"
+            boxShadow="0 0 10px black"
+            padding="0 2rem"
+            alignItems="center">
+            <Image
+                height="4rem"
+                width="auto"
+                src='/primallogo.jpg'
+                alt='logo'>
+            </Image>
+            <ul className={styles.navigationitems}>
+
+                <ButtonGroup>
+                    <IconButton
+                        icon={<InstagramIcon />}
+                        aria-label='instagram'
+                        bgColor='brown.200'
+                        _hover={
+                            {
+                                bgColor: 'brown.700'
+                            }
+                        } />
+                    <IconButton
+                        icon={<FacebookIcon />}
+                        aria-label='facebook'
+                        bgColor='brown.200'
+                        _hover={
+                            {
+                                bgColor: 'brown.700'
+                            }
+                        } />
+                </ButtonGroup>
+            </ul>
+        </Box>
+    )
+}
---
+++ styles/theme.ts
@@ -0,0 +1,27 @@
+// theme.js
+
+// 1. import `extendTheme` function
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+
+// 2. Add your color mode config
+const config: ThemeConfig = {
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+
+}
+
+// 3. extend the theme
+const theme = extendTheme({
+    config,
+    colors: {
+        brown: {
+            900: '#452821',
+            800: '#672212',
+            700: '#BC6850',
+            200: '#C3B4B1',
+            100: '#F3EBE3'
+        }
+    },
+})
+
+export default theme
