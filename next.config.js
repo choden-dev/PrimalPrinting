37068d50f9a48a7d6b78dc0c26420ff9ef156072
bcho892 <bcho892@aucklanduni.ac.nz>
2023-01-30 21:43:00 +1300

    revert changes

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-
 }
 
-module.exports = {
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
-}
+module.exports = nextConfig
