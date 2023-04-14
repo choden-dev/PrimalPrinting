443fca62e3f9bd0bf2c3375d1adf8e1ce08c9b44
bcho892 <bcho892@aucklanduni.ac.nz>
2023-04-14 23:09:49 +1200

    small preparations for redesign

--- pages/order.tsx
+++ pages/order.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from "next";
 import { Box, Heading } from "@chakra-ui/react";
+import NavBar from "../components/navbar/NavBar";
 const Order: NextPage = () => {
-  return (
-    <>
-      <Box>test</Box>
-    </>
-  );
+    return (
+        <>
+            <NavBar />
+            <Box>test</Box>
+        </>
+    );
 };
 export default Order;
