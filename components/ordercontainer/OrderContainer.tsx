5e112d97d57344354b3810b04ce7db948f7d94e4
bcho892 <bcho892@aucklanduni.ac.nz>
2023-04-16 00:10:07 +1200

    add quoting section for order

--- components/ordercontainer/OrderContainer.tsx
+++ components/ordercontainer/OrderContainer.tsx
@@ -6,50 +6,57 @@ const OrderContainer = () => {
     return (
         <>
             <Box
-                bg="white"
-                padding="1rem"
-                position="relative"
-                overflowX="visible"
+                display="grid"
+                columnGap="1rem"
+                gridTemplateColumns="3fr 1.5fr"
             >
                 <Box
-                    position="absolute"
-                    top="0"
-                    left="-2rem"
-                    h="100%"
-                    w="2.8rem"
-                    overflowY="hidden"
-                    backgroundImage="/binder.png"
-                ></Box>
-                <Box display="flex" flexDir="column" gap="1rem">
-                    <Box>
-                        <ProductCard
-                            productName=""
-                            productPrice=""
-                            productDescription=""
-                            image=""
-                            hasButton={true}
-                        />
-                    </Box>
+                    bg="white"
+                    padding="1rem"
+                    position="relative"
+                    overflowX="visible"
+                >
                     <Box
-                        w="100%"
-                        h="10rem"
-                        bg="brown.100"
-                        borderRadius="2px"
+                        position="absolute"
+                        top="0"
+                        left="-2rem"
+                        h="100%"
+                        w="2.8rem"
+                        overflowY="hidden"
+                        backgroundImage="/binder.png"
                     ></Box>
-                    <FormControl>
-                        <Box display="flex" flexDir="column" gap="1rem">
-                            <Box
-                                display="grid"
-                                gridTemplateColumns="1fr 1fr"
-                                columnGap="1rem"
-                            >
-                                <Input type="text" />
-                                <Input type="email" />
-                            </Box>
-                            <Textarea />
+                    <Box display="flex" flexDir="column" gap="1rem">
+                        <Box>
+                            <ProductCard
+                                productName=""
+                                productPrice=""
+                                productDescription=""
+                                image=""
+                                hasButton={true}
+                            />
                         </Box>
-                    </FormControl>
+                        <Box
+                            w="100%"
+                            h="10rem"
+                            bg="brown.100"
+                            borderRadius="2px"
+                        ></Box>
+                        <FormControl>
+                            <Box display="flex" flexDir="column" gap="1rem">
+                                <Box
+                                    display="grid"
+                                    gridTemplateColumns="1fr 1fr"
+                                    columnGap="1rem"
+                                >
+                                    <Input type="text" />
+                                    <Input type="email" />
+                                </Box>
+                                <Textarea />
+                            </Box>
+                        </FormControl>
+                    </Box>
                 </Box>
+                <Box w="100%" bg="white" h="13rem"></Box>
             </Box>
             <Footer />
         </>
