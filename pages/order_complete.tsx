78940731c320c71b6092f133c0bffb43797963ea
bcho892 <bcho892@aucklanduni.ac.nz>
2023-04-23 15:23:37 +1200

    fix rounding issue

--- pages/order_complete.tsx
+++ pages/order_complete.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { Box, Heading, Text, List, ListItem, Divider } from "@chakra-ui/react";
 import NavBar from "../components/navbar/NavBar";
+import Footer from "../components/footer/Footer";
 import OrderContainer from "../components/ordercontainer/OrderContainer";
 const OrderComplete: NextPage = () => {
     const router = useRouter();
@@ -12,52 +13,68 @@ const OrderComplete: NextPage = () => {
         orderItems.map((item) => {
             sum += item.cost;
         });
-        return sum;
+        return sum.toFixed(2);
     };
     console.log(orderItems);
     return (
-        <Box className="container">
-            <NavBar />
-            <Box
-                maxWidth="1100px"
-                bg="white"
-                display="flex"
-                flexDir="column"
-                padding="1rem"
-            >
-                <Heading textAlign="center">{orderId}</Heading>
-                <Divider margin="1rem 0" />
-                <Text textAlign="center" fontSize="1.5rem">
-                    Thank you for your order, here are the details:
-                </Text>
-                <List>
-                    <Box display="flex" flexDir="column" alignItems="center">
-                        {orderItems &&
-                            orderItems.map((item) => {
-                                return (
-                                    <ListItem fontSize="1.2rem" key={item.name}>
-                                        <Box display="flex">
-                                            <Text>
-                                                {item.name} x {item.quantity}
-                                            </Text>
-                                            <Text marginLeft="1rem">
-                                                {item.cost}
-                                            </Text>
-                                        </Box>
-                                    </ListItem>
-                                );
-                            })}
-                    </Box>
-                </List>
-                <Divider margin="1rem 0" />
-                <Text fontSize="1.5rem">Total: {sum()}</Text>
-                <Text>
-                    {" "}
-                    Please transfer this to the account {} with the reference{" "}
-                    {orderId}{" "}
-                </Text>
+        <>
+            <Box className="container">
+                <NavBar />
+                <Box
+                    justifySelf="center"
+                    marginTop="2rem"
+                    maxWidth="1100px"
+                    bg="white"
+                    display="flex"
+                    flexDir="column"
+                    padding="1rem"
+                >
+                    <Heading textAlign="center">{orderId}</Heading>
+                    <Divider margin="1rem 0" />
+                    <Text textAlign="center" fontSize="1.5rem">
+                        Thank you for your order, here are the details:
+                    </Text>
+                    <List>
+                        <Box
+                            display="flex"
+                            flexDir="column"
+                            alignItems="center"
+                        >
+                            {orderItems &&
+                                orderItems.map((item) => {
+                                    return (
+                                        <ListItem
+                                            fontSize="1.2rem"
+                                            fontWeight="800"
+                                            key={item.name}
+                                        >
+                                            <Box display="flex">
+                                                <Text>
+                                                    {item.name} x{" "}
+                                                    {item.quantity}
+                                                </Text>
+                                                <Text marginLeft="1rem">
+                                                    {item.cost}
+                                                </Text>
+                                            </Box>
+                                        </ListItem>
+                                    );
+                                })}
+                        </Box>
+                    </List>
+                    <Divider margin="1rem 0" />
+                    <Text fontSize="1.5rem">Total: ${sum()}</Text>
+                    <Text>
+                        {" "}
+                        Please transfer this to the account <strong>
+                            {}
+                        </strong>{" "}
+                        with the reference <strong>{orderId}</strong>
+                    </Text>
+                </Box>
+                <Footer />
             </Box>
-        </Box>
+        </>
     );
 };
 export default OrderComplete;
