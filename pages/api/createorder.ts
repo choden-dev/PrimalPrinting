122b6d16a8d7a3f3dbbc091671b60951bac1233d
bcho892 <bcho892@aucklanduni.ac.nz>
2023-04-23 13:05:54 +1200

    update id generator

--- pages/api/createorder.ts
+++ pages/api/createorder.ts
@@ -1,13 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
+import { guidGenerator } from "../../lib/utils";
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     try {
+        const orderId = guidGenerator();
         const orderRow = JSON.parse(req.body);
+
         return res.json({
-            message: orderRow,
+            message: orderId,
             success: true,
         });
     } catch (error) {
