83cc6964d9227eb45c41c22e8d2338190ae3b615
bcho892 <bcho892@aucklanduni.ac.nz>
2023-04-25 14:42:03 +1200

    check for undefined

--- pages/order_complete.tsx
+++ pages/order_complete.tsx
@@ -8,7 +8,9 @@ import OrderContainer from "../components/ordercontainer/OrderContainer";
 const OrderComplete: NextPage = () => {
     const router = useRouter();
     const orderId = router.query.orderId;
-    const orderItems = JSON.parse(router.query.items);
+    const orderItems = JSON.parse(router.query.items)
+        ? JSON.parse(router.query.items)
+        : [];
     const sum = () => {
         let sum = 0;
         orderItems.map((item) => {
