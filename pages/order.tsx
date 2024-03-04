267fe15bfb6266f4fcba95289143efa391c86ff3
bcho892 <bcho892@aucklanduni.ac.nz>
2024-03-04 16:56:44 +1300

    update meta for order

--- pages/order.tsx
+++ pages/order.tsx
@@ -3,6 +3,7 @@ import { Box, Heading } from "@chakra-ui/react";
 import NavBar from "../components/navbar/NavBar";
 import OrderContainer from "../components/ordercontainer/OrderContainer";
 import { getPackages, findPrice } from "../lib/stripe";
+import Head from "next/head";
 export async function getStaticProps() {
   /* TODO: renable after packages are back
   let packages = await getPackages();
@@ -24,10 +25,26 @@ export async function getStaticProps() {
 }
 const Order: NextPage = ({ packages }) => {
   return (
-    <Box className="container">
-      <NavBar />
-      <OrderContainer packages={packages.data} />
-    </Box>
+    <>
+      <Head>
+        <title>Primal Printing - Order</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Primal Printing - Order" />
+        <meta
+          property="og:description"
+          content="Want to get a coursebook printed? Upload it here!"
+        />
+        <meta property="og:url" content="https://primalprinting.co.nz/order" />
+        <meta
+          property="og:image"
+          content="https://primalprinting.co.nz/primallogo.png"
+        />
+      </Head>
+      <Box className="container">
+        <NavBar />
+        <OrderContainer packages={packages.data} />
+      </Box>
+    </>
   );
 };
 export default Order;
